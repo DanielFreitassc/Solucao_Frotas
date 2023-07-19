c0dac6f2c12e6e7ccf654a3fd51e2dedc624db60
Daniel Freitas <danielfreitassc@gmail.com>
2023-07-19 11:38:11 -0300

    att

---
+++ exercicios/ex09.php
@@ -0,0 +1,6 @@
+<?php
+    $prod = "leite";
+    $preco = 4.5;
+    printf("O %s está custando R$ %.2f", $prod,$preco);
+
+?>
---
+++ exercicios/ex11.php
@@ -0,0 +1,5 @@
+<?php
+    $t = "Função word wrap";
+    $r = wordwrap($t,2,"<br/>");
+    echo $r;
+?>
