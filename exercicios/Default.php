cbf92525a148f68be8651aeb415166a46e36ae61
Daniel Freitas <danielfreitassc@gmail.com>
2023-07-20 12:22:12 -0300

    ex

---
+++ exercicios/Default.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicios</title>
+</head>
+<style>
+     body{
+            background-color:pink;
+        }
+        h1{
+            color:white;
+            font-size:40px
+                        
+        }
+        #central{
+            display:flex;  
+            flex-direction:column;
+            align-items:center;          
+            background-color:gray;
+            height:700px;
+            width:700px;
+            margin-left:auto;
+            margin-right:auto;
+            margin-top:50px;
+            border-radius:30px;
+           
+        }
+        #textinput{
+            color:gray;
+        }
+        h2{
+            display:flex;
+            width:50px;
+            flex-direction:column;
+            align-items:left;
+            color:white;
+            text-align:end;
+            margin-left:auto;
+            margin-right:600px;
+            justify-content:space-evenly;
+        }
+</style>
+<body>
+    <div id="central">
+        <h1>Exercicios</h1>
+    </div>
+</body>
+</html>
---
+++ exercicios/ex12.php
@@ -0,0 +1,6 @@
+<?php
+$num1 = 2;
+$num2 = 2;
+$soma = $num1 + $num2;
+printf("A soma dos valores é %.2f",$soma)
+?>
